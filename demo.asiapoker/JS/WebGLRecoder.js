ae5d5228c589c8040352f637ab89056d953d6ba3
wu11158001 <107042097+wu11158001@users.noreply.github.com>
2024-06-29 03:08:27 +0800

    Add files via upload

--- demo.asiapoker/JS/WebGLRecoder.js
+++ demo.asiapoker/JS/WebGLRecoder.js
@@ -1,4 +1,83 @@
-function TestFunc()
-{
-    console.log("Test OK!");
+var gameContainer = document.getElementById('gameContainer');
+var recordedVideo = document.getElementById('recordedVideo');
+var recordingSelect = document.getElementById('recordingSelect');
+var mediaRecorder;
+var recordedBlobs = [];
+var recordings = [];
+var recordingCount = 0;
+
+//影片開始錄製
+function startRecording() {
+    var canvas = gameContainer.querySelector('canvas');
+    if (canvas) {
+        var stream = canvas.captureStream(30);
+        mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
+        mediaRecorder.ondataavailable = handleDataAvailable;
+        mediaRecorder.start();
+        console.log('Recording started');
+    } else {
+        console.error('Canvas element not found!');
+    }
+}
+
+//影片停止錄製
+function stopRecording() {
+    if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+        mediaRecorder.stop();
+        console.log('Recording stopped');
+    }
+}
+
+function handleDataAvailable(event) {
+    if (event.data.size > 0) {
+        recordedBlobs.push(event.data);
+    }
+}
+
+//影片錄製存檔
+function saveRecording() {
+    var blob = new Blob(recordedBlobs, { type: 'video/webm' });
+    recordings.push(blob);
+    var url = window.URL.createObjectURL(blob);
+
+    // 更新录制选择器
+    var option = document.createElement('option');
+    option.value = recordingCount;
+    option.text = `Recording ${recordingCount + 1}`;
+    recordingSelect.appendChild(option);
+    recordingSelect.style.display = 'block';
+
+    recordedBlobs = [];
+    recordingCount++;
+    if (recordingCount >= 3) {
+        Unity.call('DisableStartButton');
+    }
+}
+
+//影片暫停錄製
+function pauseRecording() {
+    if (mediaRecorder && mediaRecorder.state === 'recording') {
+        mediaRecorder.pause();
+        console.log('Recording paused');
+    }
+}
+
+//影片繼續錄製
+function resumeRecording() {
+    if (mediaRecorder && mediaRecorder.state === 'paused') {
+        mediaRecorder.resume();
+        console.log('Recording resumed');
+    }
+}
+
+//播放錄製影片
+function playRecording() {
+    var selectedRecording = recordingSelect.value;
+    if (selectedRecording !== "") {
+        var blob = recordings[selectedRecording];
+        var url = window.URL.createObjectURL(blob);
+        recordedVideo.src = url;
+        recordedVideo.style.display = 'block';
+        recordedVideo.play();
+    }
 }
