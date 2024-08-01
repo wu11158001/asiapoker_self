e771c5a8b1ec5c6ca9d7d6f71db52bd571edcf5d
jimmy <jimmy@kooco.com.tw>
2024-08-02 06:37:50 +0800

    c

--- demo.asiapoker/Firebase/FirebaseFun.js
+++ demo.asiapoker/Firebase/FirebaseFun.js
@@ -103,5 +103,6 @@ function verifyCode(code, type) {
 // 初始化 Firebase 和 Recaptcha 當文檔加載完成時
 document.addEventListener('DOMContentLoaded', (event) => {
     initializeFirebase();
-    console.log('Firebase initialized and setup completed!!');
+    setupRecaptchaVerifier();
+    console.log('Firebase and Recaptcha initialized and setup completed!!');
 });
